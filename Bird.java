3266f1e41f2b83b6846c27352b711f73de67144f
MaryProdanyuk <mary.kuzenko@gmail.com>
2017-04-30 21:08:49 +0300

    module 3 ex1

---
+++ Bird.java
@@ -0,0 +1,10 @@
+package hw3;
+
+/**
+ * Created by Admin on 30.04.2017.
+ */
+public class Bird {
+    public void sing (String text) {
+        System.out.println("I am" + text);
+    }
+}
---
+++ Main.java
@@ -0,0 +1,14 @@
+package hw3;
+
+/**
+ * Created by Admin on 30.04.2017.
+ */
+public class Main {
+    public static void main (String[] args) {
+        Bird bird = new Bird();
+        bird.sing(" walking");
+        bird.sing(" flying");
+        bird.sing(" singing");
+        bird.sing(" Bird");
+    }
+}
